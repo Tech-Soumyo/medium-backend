e961a2f0cba7ca4254f86f72f393eebb7a8deca5
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-08 03:39:57 +0530

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,16 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
-import { jwt, sign, verify } from "hono/jwt";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blogs";
+import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
-  Variables: {
-    userId: string;
-  };
 }>();
-
+app.use("/*", cors());
 app.route("/api/v1/user", userRouter);
-app.route("/api/v1/user", blogRouter);
+app.route("/api/v1/blog", blogRouter);
 
-app.get("/", (c) => {
-  return c.text("Blog Id!");
-});
 export default app;
