7a45586f7fb71524169edd21e5b4447f8199d078
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-08 03:39:40 +0530

    fix bug at signup route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -11,40 +11,6 @@ export const userRouter = new Hono<{
   };
 }>();
 
-// userRouter.post("/signup", async (c) => {
-//   const body = await c.req.json();
-//   // const { success } = signupInput.safeParse(body);
-//   // if (!success) {
-//   //   c.status(411);
-//   //   return c.json({
-//   //     message: "Inputs not correct",
-//   //   });
-//   // }
-//   const prisma = new PrismaClient({
-//     datasourceUrl: c.env.DATABASE_URL,
-//   }).$extends(withAccelerate());
-
-//   try {
-//     const user = await prisma.user.create({
-//       data: {
-//         email: body.email,
-//         password: body.password,
-//       },
-//     });
-//     const jwt = await sign(
-//       {
-//         id: user.id,
-//       },
-//       c.env.JWT_SECRET
-//     );
-
-//     return c.text(jwt);
-//   } catch (e) {
-//     console.log(e);
-//     c.status(411);
-//     return c.text("Invalid");
-//   }
-// });
 userRouter.post("/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
@@ -115,46 +81,3 @@ userRouter.post("/signin", async (c) => {
     return c.text("Invalid");
   }
 });
-// import { PrismaClient } from "@prisma/client/extension";
-// import { withAccelerate } from "@prisma/extension-accelerate";
-// import { Hono } from "hono";
-// import { sign } from "hono/jwt";
-
-// export const userRouter = new Hono<{
-//   Bindings: {
-//     DATABASE_URL: string;
-//     JWT_SECRET: string;
-//   };
-//   Variables: {
-//     userId: string;
-//   };
-// }>();
-
-// userRouter.post("/signup", async (c) => {
-//   const prisma = new PrismaClient({
-//     datasourceUrl: c.env?.DATABASE_URL,
-//   }).$extends(withAccelerate());
-
-//   const body = await c.req.json();
-//   try {
-//     const user = await prisma.user.create({
-//       data: {
-//         email: body.email,
-//         password: body.password,
-//       },
-//     });
-//     const token = await sign({ id: user.id }, c.env.JWT_SECRET);
-//     return c.json({ jwt: token });
-//   } catch (e) {
-//     console.error("Signup Error:", e); // Log the actual error
-//     c.status(403);
-//     if (e instanceof Error) {
-//       return c.json({ error: "error while signing up", details: e.message });
-//     } else {
-//       return c.json({
-//         error: "error while signing up",
-//         details: "Unknown error occurred",
-//       });
-//     }
-//   }
-// });
