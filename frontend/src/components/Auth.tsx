4d88db302d8d2219a03c0b0acdc4c04a60deb33d
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-10 00:23:09 +0530

    Add Auth.tsx and also update from its old version

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,148 @@
+import { ChangeEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { SignupInput } from "@devs-project/medium-common";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const navigate = useNavigate();
+  const [postInputs, setPostInputs] = useState<SignupInput>({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  async function sendRequest() {
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
+        postInputs,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const jwt = response.data.jwt;
+      localStorage.setItem("token", jwt);
+      navigate("/blogs");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        // Axios error
+        console.error(
+          `Axios Error: ${error.response?.status}`,
+          error.response?.data
+        );
+        alert(
+          `Error ${error.response?.status}: ${
+            error.response?.data.message || "An error occurred."
+          }`
+        );
+      } else if (error instanceof Error) {
+        // General JavaScript error
+        console.error("Error:", error.message);
+        alert("Error: " + error.message);
+      } else {
+        // Fallback for unexpected error types
+        console.error("Unexpected Error:", error);
+        alert("An unexpected error occurred.");
+      }
+    }
+    // alert the user here that the request failed
+  }
+
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-500">
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <Link
+                className="pl-2 underline"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Sign up" : "Sign in"}
+              </Link>
+            </div>
+          </div>
+          <div className="pt-8">
+            {type === "signup" ? (
+              <LabelledInput
+                label="Name"
+                placeholder="Harkirat Singh..."
+                onChange={(e) => {
+                  setPostInputs({
+                    ...postInputs,
+                    name: e.target.value,
+                  });
+                }}
+              />
+            ) : null}
+            <LabelledInput
+              label="Username"
+              placeholder="harkirat@gmail.com"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  email: e.target.value,
+                });
+              }}
+            />
+            <LabelledInput
+              label="Password"
+              type={"password"}
+              placeholder="123456"
+              onChange={(e) => {
+                setPostInputs({
+                  ...postInputs,
+                  password: e.target.value,
+                });
+              }}
+            />
+            <button
+              onClick={sendRequest}
+              type="button"
+              className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign up" : "Sign in"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+
+function LabelledInput({
+  label,
+  placeholder,
+  onChange,
+  type,
+}: LabelledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm text-black font-semibold pt-4">
+        {label}
+      </label>
+      <input
+        onChange={onChange}
+        type={type || "text"}
+        id="first_name"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
