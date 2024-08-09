6a5134d5a92a1c21b4f0b5699474efc298dbb52b
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-10 05:09:45 +0530

    export type change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signup";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 import Blog from "./pages/Blog";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blog />} />
         </Routes>
       </BrowserRouter>
     </>
