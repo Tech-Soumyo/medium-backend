4a417a5e8ec4dbb26503eab54bd869121cc345d0
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-11 21:50:08 +0530

    blogs

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,3 +1,44 @@
-export function Blogs() {
-  return <div>Blogs</div>;
-}
+import { Appbar } from "../components/AppBar";
+import { BlogCard } from "../components/BlogCard";
+import { BlogSkeleton } from "../components/BlogSkeleton";
+import { useBlogs } from "../hooks";
+
+export const Blogs = () => {
+  const { loading, blogs } = useBlogs();
+
+  // if (loading) {
+  //   return (
+  //     <div>
+  //       <Appbar />
+  //       <div className="flex justify-center">
+  //         <div>
+  //           <BlogSkeleton />
+  //           <BlogSkeleton />
+  //           <BlogSkeleton />
+  //           <BlogSkeleton />
+  //           <BlogSkeleton />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  return (
+    <div>
+      <Appbar />
+      <div className="flex justify-center">
+        <div>
+          {blogs.map((blog: any) => (
+            <BlogCard
+              id={blog.id}
+              authorName={blog.author.name || "Anonymous"}
+              title={blog.title}
+              content={blog.content}
+              publishedDate={"2nd Feb 2024"}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
