8d0bd1e8c6e84e66e6453e836375e9c764c366f7
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-08-12 22:09:42 +0530

    Full Blog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
-import Blog from "./pages/Blog";
+import { Blog } from "./pages/Blog";
 import { Blogs } from "./pages/Blogs";
 
 function App() {
