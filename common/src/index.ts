456eb21250983ddefa15e7a8385785a13173e685
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-10-06 05:46:49 +0530

    change to username

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,11 +1,11 @@
 import z from "zod";
 export const signupInput = z.object({
-  email: z.string().email(),
+  username: z.string().email(),
   password: z.string().min(6),
   name: z.string().optional(),
 });
 export const signinInput = z.object({
-  email: z.string().email(),
+  username: z.string().email(),
   password: z.string().min(6),
 });
 
