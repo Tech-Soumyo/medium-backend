f4fb06dbad8476833abcf542accd01b8b6522ff9
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-12 17:43:38 +0530

    re deploy

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,7 @@
-export const BACKEND_URL = "https://backend.soumyodeepdutta360.workers.dev";
+// export const BACKEND_URL = "https://medium-backend-2.meedium.workers.dev";
+export const BACKEND_URL = "http://127.0.0.1:8787";
+// export const BACKEND_URL =
+//   "https://medium-backend-2.soumyodeepdutta360.workers.dev";
+
+// export const AI_URL = "AIzaSyDAyiMoen1zf8vjyKYf7f0gxQRmzyvG3fs";
+export const AI_URL = "AIzaSyDJg7O7BFg9K0x2NOvAj-7iFihGjHE6feQ";
