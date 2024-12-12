dd134125dd969b574159acf27c657456d18ed314
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-12 17:44:41 +0530

    re deploy

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,10 +1,11 @@
 import { Appbar } from "../components/AppBar";
 import { BlogCard } from "../components/BlogCard";
 import { BlogSkeleton } from "../components/BlogSkeleton";
-import { useBlogs } from "../hooks";
+import { useBlogs } from "../hooks/useBlog";
 
 export const Blogs = () => {
   const { loading, blogs } = useBlogs();
+  const publishedDateString = new Date(Date.now()).toLocaleDateString("en-US");
 
   if (loading) {
     return (
@@ -26,20 +27,20 @@ export const Blogs = () => {
   return (
     <div>
       <Appbar />
-      <div className="flex justify-center">
-        <div className={`min-w-${96}`}>
-          {blogs.map((blog: any) => (
-            <BlogCard
-              id={blog.id}
-              authorName={blog.author.name || "Anonymous"}
-              title={blog.title}
-              content={blog.content}
-              publishedDate={"2nd Feb 2024"}
-              key={blog.id}
-            />
-          ))}
-        </div>
+      <div className="space-y-8 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl">
+        {/* <div className={`min-w-${96}`}> */}
+        {blogs.map((blog: any) => (
+          <BlogCard
+            id={blog.id}
+            authorName={blog.author.name || "Anonymous"}
+            title={blog.title}
+            content={blog.content}
+            publishedDate={publishedDateString}
+            key={blog.id}
+          />
+        ))}
       </div>
     </div>
+    // </div>
   );
 };
