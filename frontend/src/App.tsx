89705df5ab2b17e53af17d2b2392e26245d033d6
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-12 17:43:34 +0530

    re deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
-import { Blog } from "./pages/Blog";
 import { Blogs } from "./pages/Blogs";
+import { Blog } from "./pages/Blog";
 import { Publish } from "./pages/Publish";
+import { UpdateBlog } from "./pages/UpdateBlog";
+import { GenerativeAi } from "./pages/GenAi";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Blogs />}></Route>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+
           <Route path="/blog/:id" element={<Blog />} />
+
           <Route path="/blogs" element={<Blogs />} />
+
           <Route path="/publish" element={<Publish />} />
+          <Route path="/update/:id" element={<UpdateBlog />} />
+          <Route path="/geneartiveAi" element={<GenerativeAi />} />
         </Routes>
       </BrowserRouter>
     </>
