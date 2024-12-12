ee6021485d7bf93283cfde2570de48b5ade03720
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-12 17:44:38 +0530

    re deploy

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
-import { useBlog } from "../hooks";
 import { Appbar } from "../components/AppBar";
-import { Spinner } from "../components/Spinner";
 import { FullBlog } from "../components/FullBlog";
+import { Spinner } from "../components/Spinner";
+import { useBlog } from "../hooks/useBlog";
+import { useParams } from "react-router-dom";
 
-export function Blog() {
+// atomFamilies/selectorFamilies
+export const Blog = () => {
   const { id } = useParams();
   const { loading, blog } = useBlog({
     id: id || "",
@@ -14,18 +15,18 @@ export function Blog() {
     return (
       <div>
         <Appbar />
+
         <div className="h-screen flex flex-col justify-center">
-          <div className=" flex justify-center">
+          <div className="flex justify-center">
             <Spinner />
           </div>
         </div>
       </div>
     );
   }
-
   return (
     <div>
-      <FullBlog blog={blog} />
+      <FullBlog {...blog} />
     </div>
   );
-}
+};
